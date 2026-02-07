b3d82bdbea252c2e55bd9062be62320a79757d8b
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-07 17:25:27 +0800

    4. Updated Gitignore [For Upcoming file upload feature], Created RegisterController, Created Reposiries for Data Access, Minor updates in Views and Models.

--- Register.cs
+++ Register.cs
@@ -1,110 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace asprule1020.Models
 {
     public class Register
     {
+        [Key]
         [Required]
-        public string userName { get; set; } = string.Empty;
+        public string transId { get; set; }
+        [Required]
+        public string rule1020Id { get; set; }
+        [Required]
+        public string userName { get; set; } 
 
         [Required, EmailAddress]
-        public string estEmail { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string estEmail { get; set; } 
 
         [Required]
-        public string estPhone { get; set; } = string.Empty;
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Mobile number must be 11 digits")]
+        public string estPhone { get; set; } 
 
         [Required]
-        public string password { get; set; } = string.Empty;
+        public string password { get; set; } 
 
         [Required]
-        public string estName { get; set; } = string.Empty;
+        public string estName { get; set; } 
 
         [Required]
-        public string estType { get; set; } = string.Empty;
+        public string estType { get; set; } 
 
         [Required]
-        public string estLegalOrg { get; set; } = string.Empty;
-
+        public string estLegalOrg { get; set; } 
         public string? estLegalOrgOther { get; set; }
-
         [Required]
-        public string estRegion { get; set; } = string.Empty;
-
+        public string estRegion { get; set; } 
         [Required]
-        public string estProvince { get; set; } = string.Empty;
-
+        public string estProvince { get; set; } 
         [Required]
-        public string estCityMun { get; set; } = string.Empty;
-
+        public string estCityMun { get; set; } 
         [Required]
-        public string estBrgy { get; set; } = string.Empty;
-
+        public string estBrgy { get; set; } 
         [Required]
-        public string estStreet { get; set; } = string.Empty;
-
+        [MaxLength(150)]
+        public string estStreet { get; set; } 
         [Required]
-        public string estOwnerFirst { get; set; } = string.Empty;
-
+        [MaxLength(50)]
+        public string estOwnerFirst { get; set; } 
+        [MaxLength(50)]
         public string? estOwnerMid { get; set; }
-
         [Required]
-        public string estOwnerLast { get; set; } = string.Empty;
-
+        [MaxLength(50)]
+        public string estOwnerLast { get; set; } 
         [Required]
-        public string estCurrentCap { get; set; } = string.Empty;
-
+        public string estCurrentCap { get; set; } 
         [Required]
-        public string estTotalAssets { get; set; } = string.Empty;
-
+        public string estTotalAssets { get; set; } 
         [Required]
-        public string estBusinessNature { get; set; } = string.Empty;
-
+        public string estBusinessNature { get; set; } 
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Provide Valid Business Nature")]
         public string? estOtherBusNature { get; set; }
-
-        public int? estMaleCount { get; set; }
-
-        public int? estFemaleCount { get; set; }
-
-        public int? estTotalEmployees { get; set; }
-
+        [Required]
+        public int estMaleCount { get; set; }
+        [Required]
+        public int estFemaleCount { get; set; }
+        public int estTotalEmployees { get; set; }
         public List<string>? estTechInfo1 { get; set; }
-
         public string? estTechInfo1Other { get; set; }
-
         public List<string>? estTechInfo2 { get; set; } 
-
         public string? estTechInfo2Other { get; set; }
-
         [Required]
-        public string estChemSubstance { get; set; } = string.Empty;
-
+        public string estChemSubstance { get; set; } 
         [Required]
-        public string estSECNumber { get; set; } = string.Empty;
-
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "SEC Company Registration Number / DTI Business Name No. must be between 5 and 50 characters")]
+        public string estSECNumber { get; set; } 
+        [Required]
         public DateTime? estSECDateIssued { get; set; }
-
         [Required]
-        public string estBisPermitNumber { get; set; } = string.Empty;
-
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Business or Mayor's Permit must be between 3 and 50 characters")]
+        public string estBisPermitNumber { get; set; } 
         public DateTime? estBisPermitDateIssued { get; set; }
-
         public DateTime? estBisPermitValidityDate { get; set; }
-
         [Required]
-        public string estOwnerValidIDNumber { get; set; } = string.Empty;
-
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Business or Mayor's Permit must be between 3 and 50 characters")]
+        public string estOwnerValidIDNumber { get; set; } 
+        [MaxLength(50)]
         public DateTime? estOwnerValidIDDateIssued { get; set; }
-
         public DateTime? estOwnerValidIDDateExpire { get; set; }
-
         public DateTime? estOwnerValidIDDate { get; set; }
-
-        public string estSECFile { get; set; } = string.Empty;
-        public string estBisPermitFile { get; set; } = string.Empty;
-        public string estOwnerValidIDFile { get; set; } = string.Empty;
+        [Required]
+        public string estSECFile { get; set; }
+        [Required]
+        public string estBisPermitFile { get; set; }
+        [Required]
+        public string estOwnerValidIDFile { get; set; }
+        public string estEvalName { get; set; }
+        public DateTime estEvalDate { get; set; }
+        public string estEvalRemarks { get; set; } 
+        public string estPoHeadName { get; set; } 
+        public string estPoHeadEvalDate { get; set; } 
+        public string estPoHeadRemarks{ get; set; } 
+        public string estEvalEmailSendStatus{ get; set; } 
+        public DateTime estReopeningDate { get; set; }
+        public string estReasonForUpdate { get; set; } = string.Empty;
+        public bool estIsUpdating { get; set; } = false; 
+        public bool estIsEmailApprovedSent { get; set; } = false;
+        public DateTime estEmailApprovedSentDate { get; set; }
+        public bool estIsEmailReapplicationSent { get; set; } = true;
+        public DateTime estEmailReapplicationSentDate { get; set; }
+        public string estEvalAssinged { get; set; } = "Evaluator";
+        public string estStatus { get; set; } = "For Review";
+        public DateTime estRegistrationDate { get; set; } = DateTime.Now;
 
     }
 }
---
+++ ViewModel/RegisterVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asprule1020.Models.ViewModel
+{
+    public class RegisterVM
+    {
+        public Register? Register { get; set; }
+
+    }
+}
