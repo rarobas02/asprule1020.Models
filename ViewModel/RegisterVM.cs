4af7ab0c0b290899271c0a0b2dd244f5dcfe4c2b
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-08 21:01:28 +0800

    6. Add Register Location Input

--- ViewModel/RegisterVM.cs
+++ ViewModel/RegisterVM.cs
@@ -9,5 +9,8 @@ namespace asprule1020.Models.ViewModel
     {
         public Register? Register { get; set; }
         public IEnumerable<SelectListItem>? RegionList { get; set; }
+        public IEnumerable<SelectListItem>? ProvDistList { get; set; }
+        public IEnumerable<SelectListItem>? CityMunList { get; set; }
+        public IEnumerable<SelectListItem>? BrgyList { get; set; }
     }
 }
