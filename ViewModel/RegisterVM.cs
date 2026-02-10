4251ae7e1bafefe57bd3ebd7183d8368b5defcbb
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-10 23:04:33 +0800

    9. Hide Unhide Div

--- ViewModel/RegisterVM.cs
+++ ViewModel/RegisterVM.cs
@@ -8,10 +8,19 @@ namespace asprule1020.Models.ViewModel
 {
     public class RegisterVM
     {
-        public Register? Register { get; set; }
+        public Register Register { get; set; } = new Register();
+
         [Required]
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string Password
+        {
+            get => Register.password;
+            set => Register.password = value;
+        }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
         public string confirmPassword { get; set; }
     }
 }
