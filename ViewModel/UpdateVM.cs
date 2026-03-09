271b7bf96caabc8ed387da7495f30a4b638c65c7
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-09 21:06:09 +0800

    Update retrieve email

---
+++ ViewModel/UpdateVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asprule1020.Models.ViewModel
+{
+    public class UpdateVM
+    {
+        public Register Register { get; set; } = new Register();
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
