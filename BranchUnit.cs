5eacf365159e6de643c140134910eb176f1a14ec
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-15 22:01:01 +0800

    54. Labor Union and Branch Unit Model

---
+++ BranchUnit.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace asprule1020.Models
+{
+    public class BranchUnit
+    {
+        public Guid Id { get; set; }
+        public Guid? RegisterId { get; set; }
+        [ForeignKey("RegisterId")]
+        [ValidateNever]
+        public Register? Register { get; set; }
+        public string? Rule1020Number { get; set; }
+        public string? BranchName { get; set; }
+        public string? BranchAddress { get; set; }
+
+    }
+}
---
+++ LaborUnion.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace asprule1020.Models
+{
+    public class LaborUnion
+    {
+        public Guid Id {  get; set; }
+        public Guid? RegisterId { get; set; }
+        [ForeignKey("RegisterId")]
+        [ValidateNever]
+        public Register? Register { get; set; }
+        public string? UnionName { get; set; }
+        public string? UnionAddress { get; set; }
+        public string? UnionBLR { get; set; }
+    }
+}
