e01c801c6c1024ce64117beef78ee04908d8a304
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-04-05 21:17:04 +0800

    re-application labor unions and branch units view in item

--- ViewModel/RegisterVM.cs
+++ ViewModel/RegisterVM.cs
@@ -9,6 +9,8 @@ namespace asprule1020.Models.ViewModel
     public class RegisterVM
     {
         public Register Register { get; set; } = new Register();
+        public IEnumerable<LaborUnion> LaborUnion { get; set; } = new List<LaborUnion>();
+        public IEnumerable<BranchUnit> BranchUnit { get; set; } = new List<BranchUnit>();
         public  EvaluationChecklist CheckList{ get; set; } = new EvaluationChecklist();
         public EvaluationRemark Remarks { get; set; } = new EvaluationRemark();
     }
